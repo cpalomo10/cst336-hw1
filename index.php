ecbd239cdd099e7a18c3d4a907212ec12ddc1c60
cpalomo1 <cpalomo-ramirez@csumb.edu>
2019-07-03 12:11:58 -0700

    Pushed at HW1 files.

---
+++ index.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html>
+    <!--
+     
+     Homework 1
+     
+     -->
+    
+    <!-- This is the head -->
+    <!-- All styles and javascript go inside the head -->
+    <head>
+        <meta charset="utf-8" />
+        <title> Operation systems: A brief overview </title>
+        <link href="css/stylesheet.css" rel="stylesheet" type="text/css" />
+        <link href="https://fonts.googleapis.com/css?family=Jura&display=swap" rel="stylesheet">
+    </head>
+   
+    <body id="body1">
+        <header>
+            <h1> Operating Systems </h1>
+        
+        <hr />
+        <nav>
+            <a href="index.html">Home</a>
+            <a href="macOS.html">macOS</a>
+            <a href="windows.html">Windows</a>
+            <a href="linux.html">Linux</a>
+        </nav>
+        
+        <br />
+        
+        <div>
+            
+            <p id="para1"> <b> Overview: </b> The Operating System of a computer is one of the most, if not the most important software that runs on a computer. Anyone who has ever used a computer has used an operating system. Think of the operating system as what allows you to communicate and interact with the computer. It is also what ties together and manages all of the computers memory, processes, software, and hardware. Click the links above to learn more about some of the most popular operating systems.</p>
+            <br />
+            
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/pTdSs8kQqSA" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen>
+                </iframe>
+                
+                <h3> Main types: </h3>
+                <ol>
+                   <li> macOS </li>
+                   <li> Windows </li>
+                   <li> Linux </li>
+                </ol>
+          </header>
+        </div>
+        
+        <footer>
+            <hr>
+            CST336 Internet Programming. 2019&copy; Palomo <br />
+            <strong> Disclaimer:</strong> This webpage created for academic purposes.
+    
+        </footer>
+        
+    </body>
+    
+    <!-- This is the footer -->
+    <!-- The footer goes inside the body but not always -->
+    <footer>
+        
+        
+    </footer>
+    <!-- closing footer -->
+    
+    </body>
+    <!-- closing body -->
+    
+</html>
+
