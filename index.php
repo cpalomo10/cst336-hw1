cfa2a21d74eb344bf53be352348d07eb9c003ffa
cpalomo1 <cpalomo-ramirez@csumb.edu>
2019-07-03 14:04:28 -0700

    HW1 fixed issues.

--- index.php
+++ index.php
@@ -14,11 +14,13 @@
         <link href="css/stylesheet.css" rel="stylesheet" type="text/css" />
         <link href="https://fonts.googleapis.com/css?family=Jura&display=swap" rel="stylesheet">
     </head>
+   <!-- closing head -->
    
+   <!-- This is the body -->
     <body id="body1">
         <header>
             <h1> Operating Systems </h1>
-        
+        </header>
         <hr />
         <nav>
             <a href="index.html">Home</a>
@@ -34,16 +36,16 @@
             <p id="para1"> <b> Overview: </b> The Operating System of a computer is one of the most, if not the most important software that runs on a computer. Anyone who has ever used a computer has used an operating system. Think of the operating system as what allows you to communicate and interact with the computer. It is also what ties together and manages all of the computers memory, processes, software, and hardware. Click the links above to learn more about some of the most popular operating systems.</p>
             <br />
             
-                <iframe width="560" height="315" src="https://www.youtube.com/embed/pTdSs8kQqSA" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen>
+                <iframe width="560" height="315"
+                    src="https://www.youtube.com/embed/pTdSs8kQqSA">
                 </iframe>
                 
-                <h3> Main types: </h3>
+                <b id="b1"> Main types: </b>
                 <ol>
                    <li> macOS </li>
                    <li> Windows </li>
                    <li> Linux </li>
                 </ol>
-          </header>
         </div>
         
         <footer>
@@ -55,16 +57,6 @@
         
     </body>
     
-    <!-- This is the footer -->
-    <!-- The footer goes inside the body but not always -->
-    <footer>
-        
-        
-    </footer>
-    <!-- closing footer -->
-    
-    </body>
-    <!-- closing body -->
     
 </html>
 
